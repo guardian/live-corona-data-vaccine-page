3d4b46c2ace0a1e298dc6386c196b60ee3740b8e
josh <josh@thambi.li>
2021-04-06 16:37:47 +1000

    Added months left based on rolling average

--- SUPERSEDED_vaccineGoals4m_rolling.py
+++ SUPERSEDED_vaccineGoals4m_rolling.py
@@ -3,8 +3,45 @@ from modules.numberFormat import numberFormat
 import pandas as pd 
 import datetime 
 import numpy as np 
+import os, ssl
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
 
 oz_json = 'https://interactive.guim.co.uk/2021/02/coronavirus-widget-data/aus-vaccines.json'
+
+
+def second_builder(start, listers, which):
+    # To calculate number of days for the date range
+    num_days = 0
+    listo = []
+    for tupple in listers:
+        num_days += tupple[1]
+        # Divide total in phase by number of days in phase
+        inter = [(tupple[0]//tupple[1]) for x in range(0, tupple[1])]
+        listo.append(inter)
+    # Flatten list of lists
+    divided = [item for sublist in listo for item in sublist]
+    # Create date range
+    dates = [start + datetime.timedelta(days=x) for x in range(0, num_days)]
+    # Zip together
+    zipped = list(zip(dates, divided))
+    # Make dataframe
+    inter = pd.DataFrame(zipped)
+    #Rename
+    inter.rename(columns={0:"Date", 1:f"{which} target needed per day"}, inplace=True)
+    inter['Date'] = pd.to_datetime(inter['Date'])
+    inter[f"{which} goal"] = inter[f"{which} target needed per day"].cumsum()
+
+    return inter
+
+rollout_begin = datetime.date(2021, 2, 22)
+rollout_end = datetime.date(2021, 10, 31)
+
+chart_truncate =  datetime.date(2021, 4, 30)
+
+### ORIGINAL GOAL
 
 ## Dose counts
 
@@ -18,124 +55,112 @@ two_doses = twoa_doses + twob_doses
 
 ## Readjust doses for changing goals
 
-sixty_k = 60000
-four_mil = 4000000
+sixty_k_doses = 60000
+four_mil_doses = 4000000 - sixty_k_doses
+oneb_doses = oneb_doses + onea_doses - four_mil_doses - sixty_k_doses
 
-four_mil = four_mil - sixty_k
+# Set out timelines
 
-# (Have to minus the first two goals from oneb phase)
-oneb_doses = oneb_doses + onea_doses - four_mil - sixty_k
+sixty_end = rollout_begin + datetime.timedelta(days=7)
+sixty_delta = 7
 
-chart_truncate =  datetime.date(2021, 3, 31)
-
-rollout_begin = datetime.date(2021, 2, 22)
-rollout_end = datetime.date(2021, 10, 31)
-
-sixty_begin = rollout_begin
-sixty_end = datetime.date(2021, 3, 1)
-
-four_mil_begin = datetime.date(2021, 3, 1)
 four_mil_end = datetime.date(2021, 3, 31)
+four_mil_delta = four_mil_end - sixty_end
+four_mil_delta  = four_mil_delta.days
 
-onea_begin = rollout_begin
-onea_end = datetime.date(2021, 4, 30)
-
-# oneb_begin = datetime.date(2021, 3, 31)
-oneb_begin = datetime.date(2021, 4, 1)
+oneb_begin = four_mil_end + datetime.timedelta(days=1)
 oneb_end = datetime.date(2021, 5, 31)
-
-two_begin = datetime.date(2021, 5, 31)
-two_end = rollout_end
-
-
-## Deltas between start and end
-onea_delta = onea_end - onea_begin
-onea_delta = onea_delta.days
-
 oneb_delta = oneb_end - oneb_begin
 oneb_delta = oneb_delta.days
+
+two_begin = oneb_end + datetime.timedelta(days=1)
+two_end = rollout_end
 
 two_delta = two_end - two_begin
 two_delta = two_delta.days
 
-sixty_delta = sixty_end - sixty_begin
-sixty_delta = sixty_delta.days
+# Call function
 
-four_mil_delta = four_mil_end - four_mil_begin 
-four_mil_delta  = four_mil_delta.days
+original_rollout = [(sixty_k_doses, sixty_delta), (four_mil_doses, four_mil_delta), 
+(oneb_doses, oneb_delta), (two_doses, two_delta)]
 
+original_target = second_builder(rollout_begin, original_rollout, "Original")
 
-phases = [["sixty thousand", sixty_k, sixty_begin, sixty_end, sixty_delta],
-    ["four million", four_mil, four_mil_begin, four_mil_end, four_mil_delta], 
-        ["oneb", oneb_doses, oneb_begin, oneb_end, oneb_delta], 
-        ["two", two_doses, two_begin, two_end, two_delta]]
+### SECOND GOAL
+#source for following: https://www.health.gov.au/resources/publications/covid-19-vaccine-rollout-update-on-14-march-2021
 
-# phases = [["four million", four_mil, four_mil_begin, four_mil_end, four_mil_delta], 
-#         ["oneb", oneb_doses, oneb_begin, oneb_end, oneb_delta], 
-#         ["two", two_doses, two_begin, two_end, two_delta]]
+# Deltas are the number of days to the end of that week from the end of the previous target
+week_1_delta = 7
+week_1_target = 60000
 
-# phases = [["onea", onea_doses, onea_begin, onea_end, onea_delta], 
-#         ["oneb", oneb_doses, oneb_begin, oneb_end, oneb_delta], 
-#         ["two", two_doses, two_begin, two_end, two_delta]]
+week_2_delta = 7
+week_2_target = 120000 - week_1_target
 
-# phases = [["onea", onea_doses, onea_begin, onea_end, onea_delta]]
-# line_title = 'Phase 1a target'
+week_4_delta = 14
+week_4_target = 1000000 - week_2_target - week_1_target
 
-line_title = "4m target"
+week_6_delta = 14
+week_6_target = 2000000 - week_4_target - week_2_target - week_1_target
+
+week_8_delta = 14
+week_8_target = 3000000 - week_6_target - week_4_target - week_2_target - week_1_target
+
+week_10_delta = 14
+week_10_target = 4500000 - week_8_target - week_6_target - week_4_target - week_2_target - week_1_target
+
+second_target_rollout = [(week_1_target, week_1_delta), (week_2_target, week_2_delta), (week_4_target, week_4_delta), 
+(week_6_target, week_6_delta), (week_8_target, week_8_delta), (week_10_target, week_10_delta)]
+
+# Call function
+
+second_target = second_builder(rollout_begin, second_target_rollout, "Second")
+
+second_target['Date'] = pd.to_datetime(second_target['Date'])
+
+# Combine two target lines
+
+combo_targets = pd.merge(original_target, second_target, on="Date", how="left")
+
+## READ IN ACTUAL VACCINATIONS
 
 oz = pd.read_json(oz_json)
 oz['REPORT_DATE'] = pd.to_datetime(oz['REPORT_DATE'])
 oz = oz.sort_values(by="REPORT_DATE", ascending=True)
 
-latest_count = oz[-1:]['VACC_DOSE_CNT'].values[0]
-last_date = oz.iloc[-1:]["REPORT_DATE"].dt.strftime("%Y-%m-%d").values[0]
+## Calculate latest count
 
-oz = oz[['REPORT_DATE', 'VACC_DOSE_CNT']]
+latest_count = oz['VACC_DOSE_CNT'].max()
+last_date = datetime.datetime.strftime(oz['REPORT_DATE'].max(), "%Y-%m-%d")
+first_date = datetime.datetime.strftime(oz['REPORT_DATE'].min(), "%Y-%m-%d")
 
+oz = oz[['REPORT_DATE', 'PREV_VACC_DOSE_CNT']]
 
-def create_frame(listo):
-    ## Divide total doses for phase by number of days in phase range
-    divided = [round(listo[1]/listo[4]) for x in range(0, listo[4])]
-    ## Create list of dates for each day in phase range
-    daterange = [listo[2] + datetime.timedelta(days=x) for x in range(0, listo[4])]
-    ## Combine lists
-    zipped = list(zip(daterange, divided))
-    ## Create pandas dataframe from combined lists
-    inter = pd.DataFrame(zipped)
-    ## Add name of phase
-    inter['Phase'] = listo[0]
-    ## Rename columns
-    inter.rename(columns={0:"Date", 1:"Vaccines needed per day"}, inplace=True)
-    return inter
+## Merge current vaccine total with goal dataframes
+combo = pd.merge(combo_targets, oz, left_on="Date", right_on="REPORT_DATE", how="left")
 
-# Iterate through phases and call function on each one
-listo = [create_frame(x) for x in phases]
-
-# Combine into dataframe
-combined_phases = pd.concat(listo)
-combined_phases['Date'] = pd.to_datetime(combined_phases['Date'])
-
-combined_phases = combined_phases.groupby(['Date'])['Vaccines needed per day'].sum().reset_index()
-
-## Add current Oz vaccine total to dataframe
-combo = pd.merge(combined_phases, oz, left_on="Date", right_on="REPORT_DATE", how="left")
-
-combo[line_title] = combo['Vaccines needed per day'].cumsum()
-combo.rename(columns={'VACC_DOSE_CNT': "Doses given"}, inplace=True)
+combo.rename(columns={'PREV_VACC_DOSE_CNT': "Doses given"}, inplace=True)
 
 combo = combo.sort_values(by="Date", ascending=True)
+
+## Work out goals at cutoff
+goal_1 = round(int(combo.loc[combo['Date'] == np.datetime64(chart_truncate)]['Original goal'].values[0]), -3)
+goal_2 = round(int(combo.loc[combo['Date'] == np.datetime64(chart_truncate)]["Second goal"].values[0]), -3)
+
+# Work out vaccination gap
+# combo['Vaccination gap'] = combo["Original goal"] - combo['Doses given']
+combo['Vaccination gap'] = combo["Second goal"] - combo['Doses given']
+latest_gap = combo.loc[combo["REPORT_DATE"] == last_date]['Vaccination gap'].values[0]
+middle_gap = latest_count + latest_gap/2
 
 ## Truncate dataset at the end of April
 combo = combo.loc[combo['Date'] <= np.datetime64(chart_truncate)]
 
 combo['Date'] = combo['Date'].dt.strftime('%Y-%m-%d')
+combo.rename(columns={"Doses given":f"Doses given: {numberFormat(latest_count)}", "Second goal":"Revised goal"}, inplace=True)
+# combo.rename(columns={"Original goal":f"Original goal: {numberFormat(goal_1)}"}, inplace=True)
+# combo.rename(columns={"Second goal":f"Second goal: {numberFormat(goal_2)}"}, inplace=True)
 
-## Work out vaccination gap
-combo['Vaccination gap'] = combo[line_title] - combo['Doses given']
-combo['Vaccination gap'] = combo['Vaccination gap'].cumsum()
-
-combo.rename(columns={"Doses given":f"Doses given: {numberFormat(latest_count)}"}, inplace=True)
-# combo = combo[['Date', f"Doses given: {numberFormat(latest_count)}", line_title]]
+combo = combo[['Date', f"Doses given: {numberFormat(latest_count)}", f"Original goal", f"Revised goal"]]
 
 ## Work out rolling average
 
@@ -148,26 +173,48 @@ combo[f'{how_many_days} day rolling average'] = combo['Incremental'].rolling(how
 averager = combo.dropna()
 latest_average = averager[-1:][f'{how_many_days} day rolling average'].values[0]
 
-# combo['7 day trend'] = combo['Incremental']
-# combo['7 day trend'][0] = 20
-# combo['7 day trend'].loc[combo['Date'] > last_date] = latest_average
-# combo['7 day trend'] = round(combo['7 day trend'].cumsum())
+combo['7 day trend'] = combo['Incremental']
+combo.loc[combo['Date'] == first_date, '7 day trend'] = 20
+combo.loc[combo['Date'] > last_date, '7 day trend'] = latest_average
+combo['7 day trend'] = round(combo['7 day trend'].cumsum())
+combo.loc[combo['Date'] <= last_date, '7 day trend'] = np.nan
 
-## Cut down dataset 
+# Work out herd immunity
+# source: https://www.abs.gov.au/statistics/people/population/national-state-and-territory-population/sep-2020
+oz_pop = 25693.1 * 1000
+herd_immunity = (oz_pop*2) * 0.75
 
-combo = combo[['Date', f"Doses given: {numberFormat(latest_count)}", line_title]]
+rollout_begin = datetime.date(2021, 2, 22)
+today = datetime.datetime.today().date()
 
-# pd.set_option("display.max_rows", None, "display.max_columns", None)
+days_running = today - rollout_begin
+days_running = days_running.days
+
+
+num_days = (herd_immunity-latest_count)/combo[f'{how_many_days} day rolling average'].max()
+num_days =  round(num_days - days_running)
+
+end_date = today + datetime.timedelta(days=num_days)
+months_to_go = (end_date.year - today.year) * 12 + end_date.month - today.month
+
+
+combo = combo[['Date', f"Doses given: {numberFormat(latest_count)}", 'Original goal', 'Revised goal','7 day trend']]
+
 # print(combo)
+
+display_date = datetime.datetime.strptime(last_date, "%Y-%m-%d")
+display_date = datetime.datetime.strftime(display_date, "%d/%m/%Y")
 
 def makeTestingLine(df):
 	
     template = [
             {
                 "title": "Tracking the Covid-19 vaccine rollout in Australia",
-                "subtitle": f"Comparing the number of vaccine doses per day with the rate needed to meet the government's goals. Showing the goal of 60,000 doses within the first week, and 4m doses by the end of March. Last updated {last_date }",
+                "subtitle": f"""Showing doses administered and the federal government's <a href='https://www.theguardian.com/news/datablog/2021/feb/28/is-australias-goal-of-vaccinating-the-entire-adult-population-by-october-achievable' target='_blank'>original</a> and <a href='https://www.health.gov.au/resources/publications/covid-19-vaccine-rollout-update-on-14-march-2021' target='_blank'>revised</a> goals.<br>
+                At the 7 day average of {numberFormat(latest_average)} doses per day, <strong>it will take <red>{months_to_go}</red> more months to vaccinate</strong> 75% of the Australian population. <br>
+                <small>Last updated {display_date}.</small>""",
                 "footnote": "",
-                "source": "Covidlive.com.au, Department of Health",
+                "source": "Covidlive.com.au, Department of Health 14 March 2021 COVID-19 vaccine rollout update",
                 "dateFormat": "%Y-%m-%d",
                 "yScaleType":"",
                 "xAxisLabel": "Date",
@@ -184,10 +231,12 @@ def makeTestingLine(df):
         ]
     key = []
     periods = []
-    labels = [{"x":"2021-02-28", "y":sixty_k, "offset":100, "text":"60k target", "align":"middle"}]
-
+    # labels = []
     df.fillna("", inplace=True)
     chartData = df.to_dict('records')
+    labels = [{"x":f"{last_date}", "y":f"{middle_gap}", "offset":50, 
+    "text":f"Current gap is {numberFormat(latest_gap)}",
+     "align":"right", "direction":"right"}]
 
     yachtCharter(template=template, labels=labels, data=chartData, chartId=[{"type":"linechart"}], 
     options=[{"colorScheme":"guardian", "lineLabelling":"TRUE"}], chartName="Covid-19_oz_vaccine_tracker_4m_rolling_TEST")
