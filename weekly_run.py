c4d48b536cc35222a6c3a85cf2150bd480454b30
Josh Nicholas <hi@thambi.li>
2022-09-13 16:46:30 +1000

    Insert a day restriction

--- weekly_run.py
+++ weekly_run.py
@@ -1,13 +1,18 @@
-import weekly.WEEKLY_cases_bar
-import weekly.WEEKLY_covid_hospo_per_100k
+import datetime
 
-import weekly.WEEKLY_deaths_bar
-import weekly.WEEKLY_hospo_states_selector
+day = datetime.datetime.today().weekday()
 
-import weekly.WEEKLY_thrasher_feed
 import weekly.WEEKLY_vax_rollout
 
 import oecd_total_bar
 import new_table_incl_fourth
 
 
+if day > 4:
+  import weekly.WEEKLY_cases_bar
+  import weekly.WEEKLY_covid_hospo_per_100k
+
+  import weekly.WEEKLY_deaths_bar
+  import weekly.WEEKLY_hospo_states_selector
+
+  import weekly.WEEKLY_thrasher_feed
