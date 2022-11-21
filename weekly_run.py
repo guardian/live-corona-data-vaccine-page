99f1ed28aa0cfc4e7ce9b33ea7c4e9be7ba96802
Josh Nicholas <hi@thambi.li>
2022-11-21 14:50:58 +1100

    Print day

--- weekly_run.py
+++ weekly_run.py
@@ -1,13 +1,16 @@
 import datetime
 
 day = datetime.datetime.today().weekday()
+print("Date of the week: ", day)
 
 import oecd_total_bar
 import fixed_weekly_global_bar
 
 # day = 5
 
-if day >= 5:
+
+if day >= 0:
+
   import weekly.WEEKLY_cases_bar
   import weekly.WEEKLY_covid_hospo_per_100k
 
